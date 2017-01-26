cdc39beb5cbda23409b9124f35acc56e0dd9b1fd
Jeff Scott Brown <brownj@ociweb.com>
2017-01-26 13:36:15 -0600

    https://github.com/grails/grails-core/issues/10435

---
+++ grails-app/controllers/demo/DemoController.groovy
@@ -0,0 +1,25 @@
+package demo
+
+import grails.web.mapping.LinkGenerator
+
+import static grails.async.Promises.waitAll
+
+class DemoController {
+
+    LinkGenerator grailsLinkGenerator
+
+    def index() {
+        String link = '<unknown>'
+        def task = Person.async.task {
+            link = grailsLinkGenerator.link(controller: 'demo', action: 'foo', params: [name: 'Jeff'])
+        }
+
+        waitAll(task)
+
+        render "Link: ${link}"
+    }
+
+    def foo(String name) {
+        render "Name: $name"
+    }
+}
---
+++ grails-app/init/issue10435/BootStrap.groovy
@@ -0,0 +1,9 @@
+package issue10435
+
+class BootStrap {
+
+    def init = { servletContext ->
+    }
+    def destroy = {
+    }
+}
